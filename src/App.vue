d4dfea73798a8ecbf1ab41468d6e8c58d7876c16
Lei Pei <hileopei@gmail.com>
2019-04-15 23:31:16 -0700

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
+    <!--header-->
     <mt-header fixed title="fixed top"></mt-header>
     
     <!--footer-->
     <nav class="mui-bar mui-bar-tab">
 			<a class="mui-tab-item mui-active" href="#tabbar">
 				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-				<span class="mui-tab-label">消息</span>
+				<span class="mui-tab-label">Home</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-contact">
 				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">通讯录</span>
+				<span class="mui-tab-label">Mebership</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">Shopping Cart</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">设置</span>
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">Search</span>
 			</a>
 		</nav>
   </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 // css import
 import 'mint-ui/lib/style.css'
 import './lib/mui/dist/css/mui.min.css'
+import './lib/mui/dist/css/icons-extra.css'
 
 new Vue({
   render: h => h(App)
