f438c0cbf00d7bc4b28f02b4daa22ffd0c22d68b
Lei Pei <hileopei@gmail.com>
2019-05-04 21:37:54 -0700

    added missing files

---
+++ src/components/photos/PhotoDetail.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h3>
+      Photo Details
+    </h3>
+    <div class="photo-preview">
+      <vue-preview :slides="slide1" @close="handleClose"></vue-preview>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$nextTick(function () {
+      // $('img[itemprop]').css({
+      //   width: '60px',
+      //   height: '50px'
+      // });
+    })
+  },
+  data() {
+    return {
+      slide1: [
+          {
+            src: 'https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_b.jpg',
+            msrc: 'https://farm6.staticflickr.com/5591/15008867125_68a8ed88cc_m.jpg',
+            alt: 'picture1',
+            title: 'Image Caption 1',
+            w: 600,
+            h: 400
+          },
+          {
+            src: 'https://farm4.staticflickr.com/3902/14985871946_86abb8c56f_b.jpg',
+            msrc: 'https://farm4.staticflickr.com/3902/14985871946_86abb8c56f_m.jpg',
+            alt: 'picture2',
+            title: 'Image Caption 2',
+            w: 1200,
+            h: 900
+          }
+        ]
+    }
+  },
+  methods: {
+    handleClose() {
+      //
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.photo-preview {
+  background-color: antiquewhite;
+}
+</style>
---
+++ src/components/photos/Photolist.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="photolist-container">
+    <div id="slider" class="mui-slider">
+				<div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted" data-scroll="1">
+					<div class="mui-scroll" style="transform: translate3d(0px, 0px, 0px) translateZ(0px);">
+						<a v-for="cate in cates" :key="cate" :class="['mui-control-item', {'mui-active': cate === selected}]" @click="selected=cate" data-wid="tab-top-subpage-1.html">
+							{{cate}}
+						</a>
+					</div>
+				</div>
+			</div>
+		<div class="list-container">
+			<ul id="photo">
+				<router-link tag="li" to="/home/photolist/detail" v-for="item in list" :key="item.id">
+					<img :src="getImageUrl(item.img)" alt="">
+				</router-link>
+			</ul>
+		</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+	mounted() {
+	},
+	methods: {
+		getImageUrl(image) {
+      return require(`../../images/${image}`)
+    }
+	},
+	data() {
+		return {
+			selected: 'Fortinet',
+			cates: [
+				'Fortinet',
+				'Google',
+				'Microsoft',
+				'Amazon',
+				'SAP',
+				'Salesforce'
+			],
+			list: [{id: 1, img: 'sea3.jpeg'}, {id: 2, img: 'sea3.jpeg'}, {id: 3, img: 'sea3.jpeg'}]
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+* {
+	touch-action: pan-y;
+}
+#photo {
+	padding: 0px 10px;
+}
+.list-container {
+	ul {
+		list-style: none;
+		margin: 0%;
+		li {
+			img {
+				width: 100%;
+				height: 200px;
+				&:hover {
+				  cursor: pointer;
+			  }
+			}
+		}
+	}
+}
+</style>
